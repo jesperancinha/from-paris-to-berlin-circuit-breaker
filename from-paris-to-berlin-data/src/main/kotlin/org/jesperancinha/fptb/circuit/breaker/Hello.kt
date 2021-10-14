ac6f8f509d49454458a74d3bacf746bd783fcc6d
jesperancinha <jofisaes@gmail.com>
2021-10-14 15:20:58 +0200

    📗 City Generation

---
+++ from-paris-to-berlin-data/src/main/kotlin/org/jesperancinha/fptb/circuit/breaker/Hello.kt
@@ -0,0 +1,6 @@
+package org.jesperancinha.fptb.circuit.breaker
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ from-paris-to-berlin-data/src/test/kotlin/org/jesperancinha/fptb/circuit/breaker/HelloTest.kt
@@ -0,0 +1,8 @@
+package org.jesperancinha.fptb.circuit.breaker
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
