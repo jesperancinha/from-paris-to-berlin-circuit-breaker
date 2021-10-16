d10daafda53540649de9c86fda4ea373805281e4
Joao Esperancinha <jofisaes@gmail.com>
2021-10-16 12:16:00 +0200

    🚗 paging it up

--- from-paris-to-berlin-web/src/app/app-routing.module.ts
+++ from-paris-to-berlin-web/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FptbOverviewComponent} from "./fptb-overview/fptb-overview.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/main', pathMatch: 'full'},
+  {path: 'main', component: FptbOverviewComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
