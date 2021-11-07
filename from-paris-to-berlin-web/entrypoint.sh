807461d5ff2126956e75adac95210ddb9b76a8a7
Joao Esperancinha <jofisaes@gmail.com>
2021-11-07 11:58:12 +0100

    👾 More IP predictability

--- from-paris-to-berlin-web/entrypoint.sh
+++ from-paris-to-berlin-web/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-ip=$(getent hosts from_paris_to_berlin_web | awk '{ print $1 }')
+#ip=$(getent hosts from_paris_to_berlin_web | awk '{ print $1 }')
+ip=${FPTB_BACK_END_WEB_IP}
+
+echo "Using $ip to connect to the back end"
 
 sed -i 's/from_paris_to_berlin_web/'"$ip"'/g' /etc/nginx/conf.d/default.conf
 
