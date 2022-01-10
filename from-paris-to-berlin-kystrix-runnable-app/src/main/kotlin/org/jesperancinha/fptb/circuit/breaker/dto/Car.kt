a7470d0c7216a08e134705f67100e275fbc33982
Joao Esperancinha <jofisaes@gmail.com>
2022-01-10 12:22:33 +0100

    📦 Domain Improvement

--- from-paris-to-berlin-kystrix-runnable-app/src/main/kotlin/org/jesperancinha/fptb/circuit/breaker/dto/Car.kt
+++ from-paris-to-berlin-kystrix-runnable-app/src/main/kotlin/org/jesperancinha/fptb/circuit/breaker/dto/Car.kt
@@ -3,6 +3,4 @@ package org.jesperancinha.fptb.circuit.breaker.dto
 /**
  * Created by jofisaes on 13/10/2021
  */
-data class Car(val model: String) {
-    constructor() : this("")
-}
+data class Car(val model: String = "")
