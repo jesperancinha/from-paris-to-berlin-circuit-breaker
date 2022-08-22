824e237e17d96941a3a4e59df8635599398eaed0
Joao Esperancinha <jofisaes@gmail.com>
2022-08-22 23:37:57 +0200

    Corrects IPs

--- from-paris-to-berlin-web/entrypoint.sh
+++ from-paris-to-berlin-web/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 #ip=$(getent hosts from_paris_to_berlin_service | awk '{ print $1 }')
+
 ip=${FPTB_BACK_END_WEB_IP}
-
 echo "Using $ip to connect to the back end"
-
 sed -i 's/from_paris_to_berlin_service/'"$ip"'/g' /etc/nginx/conf.d/default.conf
+
+ip=${FPTB_BACK_END_WS_WEB_IP}
+echo "Using $ip to connect to the back end"
+sed -i 's/from_paris_to_berlin_ws_service/'"$ip"'/g' /etc/nginx/conf.d/default.conf
 
 nginx -t
 
