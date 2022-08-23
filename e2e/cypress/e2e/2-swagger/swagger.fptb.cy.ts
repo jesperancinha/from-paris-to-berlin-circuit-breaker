72b94a43697dc350a0f393185b24547211708b43
Joao Esperancinha <jofisaes@gmail.com>
2022-08-23 13:58:46 +0200

    E2E tests in the pipeline for Swagger

---
+++ e2e/cypress/e2e/2-swagger/swagger.fptb.cy.ts
@@ -0,0 +1,15 @@
+describe('Swagger Tests Webservice', () => {
+    const baseUrl = Cypress.config('baseUrl')
+    const host = Cypress.env('host') ? Cypress.env('host') : 'localhost';
+    const port = Cypress.env('port') ? Cypress.env('port') : '8080';
+
+    it('shows swagger', () => {
+        const path = `/api/fptb/webjars/swagger-ui/index.html`;
+        const baseHost = Cypress.env('host') ? `http://${host}:${port}` : baseUrl;
+        cy.visit(`${baseHost}${path}`);
+        cy.get('h2', {timeout: 10000}).contains('OpenAPI definition', {timeout: 10000}).should('not.be.null');
+        cy.wait(1000);
+        cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:9000/api/fptb')
+    });
+
+})
---
+++ e2e/cypress/e2e/2-swagger/swagger.fptb.ws.cy.ts
@@ -0,0 +1,14 @@
+describe('Swagger Tests Web Sockets', () => {
+    const baseUrl = Cypress.config('baseUrl')
+    const host = Cypress.env('host') ? Cypress.env('host') : 'localhost';
+    const port = Cypress.env('port') ? Cypress.env('port') : '8081';
+    const path = Cypress.env('port') ? '/api/fptb/broker/ws/swagger-ui/index.html' : '/api/fptb/swagger-ui/index.html';
+
+    it('shows swagger', () => {
+        let baseHost = Cypress.env('host') ? `http://${host}:${port}` : baseUrl;
+        cy.visit(`${baseHost}${path}`);
+        cy.get('h2', {timeout: 10000}).contains('OpenAPI definition', {timeout: 10000}).should('not.be.null');
+        cy.wait(1000);
+        cy.get('div[class="servers"] > label > select > option').should('have.value', 'http://localhost:9000/api/fptb')
+    });
+})
